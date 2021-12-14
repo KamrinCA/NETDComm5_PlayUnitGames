62990abc771b20d6a855a01438e44aa79e16af7b
Kamrin Aubin <kamrinaubin@outlook.com>
2021-12-13 19:49:38 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿/* Author: Kamrin Aubin
+ * Last Modified: December 13, 2021
+ * Description: Home Controller. Default home controller from creating web application.
+ *              Includes some changes to better represent the my web application.
+ * 
+ */
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NETDComm5.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+
+namespace NETDComm5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Home page of Play Unit Games Management Service site
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Added About View, Displays information regarding Play Unit Games and assignment
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿/* Author: Kamrin Aubin:
+ * Last Modified: December 13, 2021
+ * Description: Helper class, will be used to set up the identity framework database tables.
+ *              Also, sets up the other 3 tables required for the lab using DbSets
+ * 
+ */
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+// Imported the models to get access to VideoGame, VideoGameConsole, and Stores
+using NETDComm5.Models;
+
+namespace NETDComm5.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        // Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Helps us run a command to take care of CRUD operations for all models (VideoGame, VideoGameConsole, Store)
+        public DbSet<VideoGame> VideoGames { get; set; }
+        public DbSet<VideoGameConsole> VideoGameConsole { get; set; }
+        public DbSet<Store> Stores { get; set; }
+    }
+}
